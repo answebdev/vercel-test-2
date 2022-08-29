8028a72b1355437ea0374cefaea7c0885a28aa62
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-28 18:06:05 -0700

    update routing

--- api/index.js
+++ api/index.js
@@ -10,14 +10,13 @@
 // server.listen(port);
 
 const express = require('express');
-// const season = require('./mock/endpoints');
-
 const episodes = require('../routes/episodes');
 const season1 = require('../routes/season1');
 const season2 = require('../routes/season2');
 const season3 = require('../routes/season3');
 const season4 = require('../routes/season4');
 const season5 = require('../routes/season5');
+
 const cors = require('cors');
 
 //const app = express();
@@ -28,11 +27,11 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 // app.use('/api/seasons', season);
-app.use('/episodes', episodes);
-app.use('/season1', season1);
-app.use('/season2', season2);
-app.use('/season3', season3);
-app.use('/season4', season4);
-app.use('/season5', season5);
+app.use('/api/episodes', episodes);
+app.use('/api/season1', season1);
+app.use('/api/season2', season2);
+app.use('/api/season3', season3);
+app.use('/api/season4', season4);
+app.use('/api/season5', season5);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
